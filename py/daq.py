87c52ba05fa84ba84a99428172d60e40014a8b5e
Richard <rchrdgermond@gmail.com>
2017-08-03 20:14:57 -0400

    moved the main call to daq.py. included a directory for data and a helper zsh script to easily run the daq

--- py/daq.py
+++ py/daq.py
@@ -62,3 +62,10 @@ class   DAQ:
         finally:
             self.ctrl.close()
 
+def main():
+    #start the DAQ here
+    daq = DAQ('192.168.1.28',10000)         #probably change this too so that the IP address can be set through GUI or config file
+    daq.start()
+
+if __name__ == '__main__':
+    main()
